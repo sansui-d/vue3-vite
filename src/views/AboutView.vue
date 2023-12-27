41b06a4fdca12b51c41c784b902768b8e53ffea1
Dylan <71920152+sansui-d@users.noreply.github.com>
2023-12-27 19:01:50 +0800

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useCounterStore } from '@/stores/counter'
+
+const { count, doubleCount } = storeToRefs(useCounterStore())
+const { increment } = useCounterStore()
+const handleClick = () => {
+  increment()
+}
+</script>
+
+<template>
+  <div class="about">{{ count }}</div>
+  <div class="about">{{ doubleCount }}</div>
+  <div class="about" @click="handleClick">+</div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
